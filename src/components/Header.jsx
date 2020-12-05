f9c3cee4f58c625733236d86271b5f4e2ff0ba60
Conrado Remon <conradremon7@gmail.com>
2020-12-05 03:54:59 -0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles.css"
+
+export default function Header() {
+    return (
+        <div>
+            <h1>Note Keeper</h1>
+            <h3>Write down your notes and keep them safe here :) <br/>
+                And you can change the progress too!
+            </h3>
+        </div>
+    )
+}
